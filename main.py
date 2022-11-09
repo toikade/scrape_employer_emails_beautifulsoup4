698ebfb65f2e9619a48378f0a1827f1b8579b7a9
toikade <toikadeaaron@gmail.com>
2022-11-09 16:10:24 +0100

    parsed page content and pretty printed it

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://version3.guestworkervisas.com/newversionemailer.php")
+print(page.status_code)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
